bdda3d95eb7b84b99ac717724e8f8f43915395fa
Nathan Diddle <ibbignerd@yahoo.com>
2018-11-26 18:58:12 -0500

    Pull request was approved and code was merged in

--- file1.js
+++ file1.js
@@ -8,3 +8,7 @@ myFunction(){
 mySecondFunction(){
 	console.log("This is the second one");
 }
+
+myThirdFunction(){
+	console.log("This was added to develop after the feature/feature1 branch was created");
+}
